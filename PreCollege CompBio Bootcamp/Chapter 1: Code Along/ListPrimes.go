a9b90ca988035cdc75a1788a3b16bbe6d5ceb6f8
Mallhw <67278784+Mallhw@users.noreply.github.com>
2025-06-27 09:47:46 -0400

    1.7

---
+++ PreCollege CompBio Bootcamp/Chapter 1: Code Along/ListPrimes.go
@@ -0,0 +1,32 @@
+func ListPrimes(n int) []int {
+    x := SieveOfEratosthenes(n)
+    ans := make([]int, 0)
+    for i := 0; i < n+1; i++ {
+        if x[i] {
+            ans = append(ans, i)
+        }
+    }
+    return ans
+}
+
+// Hint: insert your SieveOfEratosthenes() and CrossOffMultiples() functions here.
+func SieveOfEratosthenes(n int) []bool {
+    ans := make([]bool, n+1)
+    for x := 2; x < n+1; x++ {
+        ans[x] = true
+    }
+    for i := 2; i < n+1; i++ {
+        if ans[i] == true {
+            ans = CrossOffMultiples(ans, i)
+        }
+    }
+    return ans
+}
+
+// Hint: insert your CrossOffMultiples function here.
+func CrossOffMultiples(primeBooleans []bool, p int) []bool {
+    for k := 2 * p; k < len(primeBooleans); k += p {
+        primeBooleans[k] = false
+    }
+    return primeBooleans
+}
